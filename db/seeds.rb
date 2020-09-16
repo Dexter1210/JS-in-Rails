22669ec147299be4d4fe6808abd4dce168e48fee
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-16 16:52:19 +0530

    Basic Scaffolding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Task.delete_all
+
+10.times do |index|
+Task.create(
+    title: Faker::Educator.course_name,
+    position:index,
+    completed:index % 2==0 ? true:false
+)
+end
+
